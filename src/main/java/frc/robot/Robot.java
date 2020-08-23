03b9a7905125eb8223fc512ad123892c79a2153b
LennyAbbas <lennyabbas3@gmail.com>
2020-08-22 17:55:16 -0700

    not really sure what im doing at this point

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,41 +4,32 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
+  String exampleKey = "move,2,4"; // for move(distance 2, heading 4) or something like that
+
   // have fun trying to figure out what this code does
   // I'm only about 50% sure that it actually works
   private void convertKeyToFunction(String key) {
-    final char parameterDivider = ',';
+    final String parameterDivider = ",";
     int numberOfParameters = 1;
     String[] arrOfParameters;
 
-    // figures out the number of parameters
-    if (key.length() == 1) {
-      numberOfParameters = 0;
-    } else {
-      for (int i = 0; i < key.length(); i++) {
-        if (key.charAt(i) == parameterDivider) {
-          numberOfParameters++;
-        }
-      }
-    }
-
-    // figures out what the parameters are
-    if (numberOfParameters != 0) {
-      arrOfParameters = key.substring(1).split(Character.toString(parameterDivider));
-    }
+    // splits the key into an array of strings such that the first string is a method and the following strings are parameters ex: "move" "2" "4"
+    arrOfParameters = key.split(parameterDivider);
 
     // plugs the parameters into the methods (I used shoot and move as an example)
-    if (key.charAt(0) == 's') {
-      if (numberOfParameters == 0) {
+    if (arrOfParameters[0] == "shoot") {
+      if (arrOfParameters.length == 1) { // no parameters
         shoot(); // pretend that theres actually a shoot method
       }
-    } else if (key.charAt(0) == 'm') {
-      if (numberOfParameters == 0) {
+    }
+    
+    if (arrOfParameters[0] == "move") {
+      if (arrOfParameters.length == 1) { // no parameters
         move(); // pretend that theres actually a move method
-      } else if (numberOfParameters == 1) {
-        move(Double.parseDouble(arrOfParameters[0]));
-      } else if (numberOfParameters == 2) {
-        move(Double.parseDouble(arrOfParameters[0]), Double.parseDouble(arrOfParameters[1]));
+      } else if (arrOfParameters.length == 2) { // 1 parameter
+        move(Double.parseDouble(arrOfParameters[1]));
+      } else if (arrOfParameters.length == 3) { // 2 parameters
+        move(Double.parseDouble(arrOfParameters[1]), Double.parseDouble(arrOfParameters[2]));
       }
     }
   }
